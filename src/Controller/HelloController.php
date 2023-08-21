104acb5079f06bade2d444d9a38b7c5fff86a270
Raziel Rodrigues <razielx3@live.com>
2023-08-21 01:36:18 -0300

    feat: o principal sobre roteamento

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,14 +3,35 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
 
+    private array $messages = [
+        'chama amigo!',
+        'ta ligado!',
+        'é nois'
+    ];
 
-
-    public function index()
+    // ROTA NORMAL E COM PRIORIDADE
+    #[Route('/hello', name: 'app_hello_world', priority: 2)]
+    public function index2($limit): Response
     {
         return new Response('Hello world!');
+    }
+
+    // ROTA COM PARAMETRO OPCIONAL
+    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+    public function index($limit): Response
+    {
+        return new Response(implode(', ', array_slice($this->messages, 0, $limit)));
+    }
+
+    // ROTA COM PARAMETRO OBRIGATORIO
+    #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+    public function showOne(int $id): Response
+    {
+        return new Response($this->messages[$id], 200);
     }
 }
