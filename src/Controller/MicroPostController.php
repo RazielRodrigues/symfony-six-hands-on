1314f07915c241e7c5964858571a997968e584cd
Raziel Rodrigues <razielx3@live.com>
2023-08-28 20:31:38 -0300

    selecionando dados

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MicroPostController extends AbstractController
 {
-    #[Route('/micro/post', name: 'app_micro_post')]
-    public function index(): Response
+    #[Route('/micro-post', name: 'app_micro_post')]
+    public function index(MicroPostRepository $posts): Response
     {
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $posts->findAll(),
+        ]);
+    }
+
+    // SENIO
+    #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    public function index2(MicroPost $post): Response
+    {
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
         ]);
     }
 }
