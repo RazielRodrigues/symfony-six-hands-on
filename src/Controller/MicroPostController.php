8fbae0bd79d85e3ff5f2173209013b2475af7ff0
Raziel Rodrigues <razielx3@live.com>
2023-09-02 13:17:20 -0300

    feat: working with forms

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -3,10 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\MicroPost;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class MicroPostController extends AbstractController
 {
@@ -18,12 +23,60 @@ class MicroPostController extends AbstractController
         ]);
     }
 
-    // SENIO
+    // SENSIO
     #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
     public function index2(MicroPost $post): Response
     {
         return $this->render('micro_post/show.html.twig', [
             'post' => $post,
+        ]);
+    }
+
+    #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    public function add(Request $request, MicroPostRepository $microPostRepository): Response
+    {
+        $micropost = new MicroPost();
+        $form = $this->createFormBuilder($micropost)
+            ->add('title', TextType::class)
+            ->add('text', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+
+
+            $microPostRepository->add($post, true);
+            $this->addFlash('success', 'ok inserido!');
+
+            return $this->redirectToRoute('app_micro_post');
+        }
+
+        return $this->render('micro_post/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
+    public function edit(MicroPost $post, Request $request, MicroPostRepository $microPostRepository): Response
+    {
+        $form = $this->createForm(MicroPostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $microPostRepository->add($post, true);
+
+            $this->addFlash('success', 'ok editado!');
+            return $this->redirectToRoute('app_micro_post');
+        }
+
+        return $this->render('micro_post/edit.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Repository/MicroPostRepository.php
+++ src/Repository/MicroPostRepository.php
@@ -21,28 +21,47 @@ class MicroPostRepository extends ServiceEntityRepository
         parent::__construct($registry, MicroPost::class);
     }
 
-//    /**
-//     * @return MicroPost[] Returns an array of MicroPost objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('m.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function add(MicroPost $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
 
-//    public function findOneBySomeField($value): ?MicroPost
-//    {
-//        return $this->createQueryBuilder('m')
-//            ->andWhere('m.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(MicroPost $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+
+    //    /**
+    //     * @return MicroPost[] Returns an array of MicroPost objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('m')
+    //            ->andWhere('m.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('m.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?MicroPost
+    //    {
+    //        return $this->createQueryBuilder('m')
+    //            ->andWhere('m.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
